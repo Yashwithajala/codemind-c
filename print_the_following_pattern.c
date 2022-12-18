acfab1e52c30f4d9e07dc3f13163d55aaeecd372
Yashwithajala <119839898+Yashwithajala@users.noreply.github.com>
2022-12-18 10:43:52 +0530

    #include<stdio.h> int main() {     int n,i,j,z;     scanf("%d",&n);     for(i=0;i<n;i++)     {         for(j=n;j>i+1;j--)         {             printf(" ");         }         for(z=1;z<=2*i+1;z++)         printf("%d",i+1);         printf(" ");     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,15 +1,17 @@
 #include<stdio.h>
 int main()
 {
-int n,i,j;
-scanf("%d",&n);
-for(i=n;i>0;i--)
+    int n,i,j,z;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
     {
-for(j=n;j>0;j--)
-    {
-        printf("%d ",j);
-    }
-    printf("
-");
+        for(j=n;j>i+1;j--)
+        {
+            printf(" ");
         }
+        for(z=1;z<=2*i+1;z++)
+        printf("%d",i+1);
+        printf("
+");
+    }
 }
